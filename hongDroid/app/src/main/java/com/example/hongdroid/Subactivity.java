094f88fad2e83b38ec776b59323f2cf48de60327
luckynamja <kjung10@gmail.com>
2020-07-24 17:29:49 +0900

    hongDroid exercises

---
+++ hongDroid/app/src/main/java/com/example/hongdroid/Subactivity.java
@@ -0,0 +1,13 @@
+package com.example.hongdroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Subactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
